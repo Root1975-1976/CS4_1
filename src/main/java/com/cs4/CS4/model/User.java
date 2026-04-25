a52939cc3db2751f261deb99b8f904a062646191
Esraa Elsheikh <esraaelsheikh2005@gmail.com>
2026-04-26 00:45:31 +0300

    added jpa dependencies and user model

---
+++ src/main/java/com/cs4/CS4/model/User.java
@@ -0,0 +1,22 @@
+package com.cs4.CS4.model;
+import javax.management.relation.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+@Entity
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
+}
