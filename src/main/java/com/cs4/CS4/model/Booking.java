e0ad2a58fd236d37390c3f0f797d835d5f4ecf0e
Clarice shohdy <shohdyclarice0@gmail.com>
2026-04-26 22:42:44 +0300

    message

--- src/main/java/com/cs4/CS4/model/Booking.java
+++ src/main/java/com/cs4/CS4/model/Booking.java
@@ -1,5 +1,31 @@
 package com.cs4.CS4.model;
 
-public class Booking {
+import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Booking {
+    @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Room room;
+
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
 }
+
