f65528c66346d23b64922f37117503eae4dbc749
Esraa Elsheikh <esraaelsheikh2005@gmail.com>
2026-05-07 21:58:17 +0300

    update

--- src/main/java/com/cs4/CS4/model/Review.java
+++ src/main/java/com/cs4/CS4/model/Review.java
@@ -6,45 +6,45 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 @Entity
 public class Review {
-	    @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
- private int rating;
-	    private String comment;
-	    @ManyToOne
-	    private User user;
- @ManyToOne
-	    private Room room;
- public Long getId() {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private int rating;
+private String comment;
+@ManyToOne
+private User user;
+@ManyToOne
+private Room room;
+public Long getId() {
 	return id;
  }
- public void setId(Long id) {
+public void setId(Long id) {
 	this.id = id;
  }
- public int getRating() {
+public int getRating() {
 	return rating;
  }
- public void setRating(int rating) {
+public void setRating(int rating) {
 	this.rating = rating;
  }
- public String getComment() {
+public String getComment() {
 	return comment;
  }
- public void setComment(String comment) {
+public void setComment(String comment) {
 	this.comment = comment;
  }
- public User getUser() {
+public User getUser() {
 	return user;
  }
- public void setUser(User user) {
+public void setUser(User user) {
 	this.user = user;
  }
- public Room getRoom() {
+public Room getRoom() {
 	return room;
  }
- public void setRoom(Room room) {
+public void setRoom(Room room) {
 	this.room = room;
  }
-	}
+}
 
 
---
+++ src/main/java/com/cs4/CS4/service/UserService.java
@@ -0,0 +1,4 @@
+package com.cs4.CS4.service;
+public class UserService {
+
+}
