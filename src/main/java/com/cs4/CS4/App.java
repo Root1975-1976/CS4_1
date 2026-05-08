f79b11c8c688f6b8e6c87317373fa929f4546673
Esraa Elsheikh <esraaelsheikh2005@gmail.com>
2026-05-08 22:21:24 +0300

    add

--- src/main/java/com/cs4/CS4/App.java
+++ src/main/java/com/cs4/CS4/App.java
@@ -1,13 +1,10 @@
 package com.cs4.CS4;
-
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	 public static void main(String[] args) {
+	        SpringApplication.run(App.class, args);
+	    }
 }
