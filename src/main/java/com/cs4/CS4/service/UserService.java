6dd6935522cebcf1e04768030daa5011622fa61d
Esraa Elsheikh <esraaelsheikh2005@gmail.com>
2026-05-08 19:45:48 +0300

    add

--- src/main/java/com/cs4/CS4/service/UserService.java
+++ src/main/java/com/cs4/CS4/service/UserService.java
@@ -1,4 +1,17 @@
 package com.cs4.CS4.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cs4.CS4.model.User;
+import com.cs4.CS4.repository.UserRepository;
+@Service
 public class UserService {
-
+    @Autowired
+    private UserRepository userRepo;
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
 }
