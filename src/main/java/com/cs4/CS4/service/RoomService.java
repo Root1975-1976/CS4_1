a615babe801fc40f832247365d429c944b1cda10
Clarice shohdy <shohdyclarice0@gmail.com>
2026-05-08 20:24:41 +0300

    add

---
+++ src/main/java/com/cs4/CS4/service/RoomService.java
@@ -0,0 +1,18 @@
+package com.cs4.CS4.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.cs4.CS4.model.Room;
+import com.cs4.CS4.repository.RoomRepository;
+@Service
+public class RoomService {
+ @Autowired
+    private RoomRepository roomRepo;
+    public Room saveRoom(Room room) {
+        room.setAvailable(true);
+        return roomRepo.save(room);
+    }
+    public List<Room> getAllRooms() {
+        return roomRepo.findAll();
+    }
+}
