c4c94f54e72435ced5c0c9537f391666ef6e8d62
narimanauk-cell <nariman.a.uk@gmail.com>
2026-05-08 22:12:58 +0300

    message

---
+++ src/main/java/com/cs4/CS4/controller/BookingController.java
@@ -0,0 +1,17 @@
+package com.cs4.CS4.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.cs4.CS4.service.BookingService;
+@Controller
+public class BookingController {
+	    @Autowired
+	    private BookingService bookingService;
+	    @GetMapping("/bookings")
+	    public String getBookings(Model model) {
+	        model.addAttribute("bookings", bookingService.getAllBookings());
+	        return "bookings";
+	    }
+	}
+
