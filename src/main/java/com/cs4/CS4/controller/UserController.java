58e170705a792348a5f58464898da1c7c8c75acb
janaa5158-boop <janaa5158@gmail.com>
2026-05-08 20:54:45 +0300

    message

--- src/main/java/com/cs4/CS4/controller/UserController.java
+++ src/main/java/com/cs4/CS4/controller/UserController.java
@@ -1,5 +1,16 @@
 package com.cs4.CS4.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import com.cs4.CS4.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class UserController {
-
+	@Autowired
+    private UserService userService;
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
 }
